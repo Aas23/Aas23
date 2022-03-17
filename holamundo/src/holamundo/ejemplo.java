722aa4b8ce87f3225956efbfdfb50a04c18731ff
Alvaro <alvaro_aragon@hotmail.com>
2022-03-17 21:46:09 +0100

    Mi nombre es Alvaro

--- holamundo/src/holamundo/ejemplo.java
+++ holamundo/src/holamundo/ejemplo.java
@@ -5,8 +5,6 @@ public class ejemplo {
 	public static void main(String [] args) {
 	
 		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
-		System.out.println("Hola");
-		System.out.println("Buenos dias, mi nombre es Álvaro");
 
 	}
 
