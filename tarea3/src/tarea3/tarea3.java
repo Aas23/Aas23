3111abcf99d2878777acc391129081ef2acbe9c9
Alvaro <alvaro_aragon@hotmail.com>
2022-03-21 11:03:43 +0100

    Primer Commit

---
+++ tarea3/src/tarea3/tarea3.java
@@ -0,0 +1,11 @@
+package tarea3;
+
+public class tarea3 {
+	
+	public static void main(String [] args) {
+	
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
+
+	}
+
+}
